a53258e7cdae275fa7f74f977bd03e3fa1eade1f
FilipSzkarlat <filip.szkarlat@gmail.com>
2024-02-19 13:07:20 +0100

    email validation is done and whole project is ready

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const input = document.getElementById("email");
+
 function emailValidation() {
   if (
     (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
@@ -6,9 +8,10 @@ function emailValidation() {
       input.value != "") ||
     input.value == ""
   ) {
-    errorMessage.style.display = "block";
-    input.style.border = "solid red 2px";
-    errorIcon.style.display = "block";
+    input.style.border = "solid rgb(246, 156, 156) 2px";
+    input.style.backgroundColor = "rgb(254, 235, 235)";
+    input.style.color = "red";
+    document.getElementById("error-message").style.display = "block";
 
     event.preventDefault();
   }
